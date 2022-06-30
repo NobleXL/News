e3af2eec216738e0d7a6a190257fd32b8a952f78
Noble <noblegiao@gmail.com>
2022-06-30 18:57:23 +0800

    页面初建

---
+++ app/src/main/java/com/noble/news/ui/screens/MineScreen.kt
@@ -0,0 +1,23 @@
+package com.noble.news.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * @author 小寒
+ * @version 1.0
+ * @date 2022/6/30 18:52
+ */
+
+@Composable
+fun MineScreen() {
+    Text(text = "我的页面")
+}
+
+@Preview
+@Composable
+fun MineScreenPreview() {
+    MineScreen()
+}
+
---
+++ app/src/main/java/com/noble/news/ui/screens/TaskScreen.kt
@@ -0,0 +1,23 @@
+package com.noble.news.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * @author 小寒
+ * @version 1.0
+ * @date 2022/6/30 18:51
+ */
+
+@Composable
+fun TaskScreen() {
+    Text(text = "Task Content")
+}
+
+@Preview
+@Composable
+fun TaskScreenPreview() {
+    TaskScreen()
+}
+
