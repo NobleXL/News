2849a646a2674bfceeca3c06e5d778578655c502
Noble <noblegiao@gmail.com>
2022-07-03 16:05:03 +0800

    学习任务页初建

--- app/src/main/java/com/noble/news/ui/screens/TaskScreen.kt
+++ app/src/main/java/com/noble/news/ui/screens/TaskScreen.kt
@@ -1,16 +1,17 @@
 package com.noble.news.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.noble.news.ui.components.appBarHeight
 
 /**
  * @author 小寒
@@ -20,7 +21,31 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun TaskScreen() {
-    Text(text = "Task Content")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    listOf(Color(0xFF149EE7), Color.White)
+                )
+            )
+    ) {
+        Row(
+            modifier = Modifier
+                .statusBarsPadding()
+                .height(appBarHeight),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "学习任务",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                fontSize = 18.sp
+            )
+        }
+    }
 }
 
 @Preview
